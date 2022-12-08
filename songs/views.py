ab733ade92c4f57ec5891935abaf0cc7ae188831
emilytwines <emdelbridge@gmail.com>
2022-12-08 12:49:52 -0500

    added Get and Get by ID methods to views.py + added corsheaders package

--- songs/views.py
+++ songs/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from models import Song
+from serializers import SongSerializer
+from rest_framework import status
+
+@api_view(['GET'])
+def songs_list(request):
+    return Response('ok')
+
+
+
+@api_view(['GET'])
+def song_details(request, pk):
+    try:
+        song = Song.objects.get(pk = pk)
+        serializer = SongSerializer(song)
+        return Response(serializer.data)
+    except: 
+        Song.DoesNotExist
+        return Response(status.HTTP_404_NOT_FOUND)
 
 # Create your views here.
